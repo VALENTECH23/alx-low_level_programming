c08d830ea8f1d93b0faa0585c7035bac7c6e351f
VALENTECH23 <chinonsoval99@gmail.com>
2022-10-18 07:15:33 -0700

    My 3-print_alphabets.c letter

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main-program entry point.
+ *Return:0-if no error ,else return error for nonzero.
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,1 +1,1 @@
-to print the last digit of the number stored in the variable n
+a program that prints the alphabet in lowercase, and then in uppercase, followed by a new line.
